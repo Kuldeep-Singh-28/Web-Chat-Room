1ee21042615f1f1fbe61047df62263e383c74f24
Kuldeep28-star <kuldeepsingh281999@gmail.com>
2020-12-16 00:20:47 +0530

    firebase-setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBngQwkiZR9qJo1dMucPPgIHwJt6nyaeiU",
+    authDomain: "whatsapp-kss.firebaseapp.com",
+    projectId: "whatsapp-kss",
+    storageBucket: "whatsapp-kss.appspot.com",
+    messagingSenderId: "233720422878",
+    appId: "1:233720422878:web:1dc41e57bcb0845cba3d4f",
+    measurementId: "G-4YEN6S78KC"
+  };
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="/__/firebase/8.2.0/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="/__/firebase/8.2.0/firebase-analytics.js"></script>
+
+// <!-- Initialize Firebase -->
+// <script src="/__/firebase/init.js"></script>
