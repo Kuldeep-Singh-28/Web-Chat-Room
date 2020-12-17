cc8133b75c62a8e3c013db9f8daefda29f935cc5
Kuldeep28-star <kuldeepsingh281999@gmail.com>
2020-12-17 19:05:40 +0530

    firebase-I

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -26,7 +26,8 @@ function Login() {
             <div className="login__container">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/225px-WhatsApp.svg.png" alt="" />
                 <div className="login__text">
-                    <h1>Sign in to whatsapp</h1>
+                    <h1>Sign in to whatsapp-web</h1>
+                    <p>chat-room</p>
                 </div>
                     <Button onClick={signIn}>Sign in with Google</Button>
             </div>
